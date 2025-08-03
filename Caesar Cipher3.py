d31c702f2d57b08aebd09e0f6bdf6ed2a55ace58
samy9414 <bambal.sameer@gmail.com>
2025-08-04 00:03:41 +0200

    caesar modified

--- Caesar Cipher3.py
+++ Caesar Cipher3.py
@@ -3,9 +3,6 @@ import art
 print(art.logo)
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']
-
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
     if encode_or_decode == "decode":
