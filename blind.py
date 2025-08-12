8a9bacf5b54d54ce41efaa4744aea41c6599aad6
samy9414 <bambal.sameer@gmail.com>
2025-08-12 21:32:42 +0200

    Blind auction par modified

--- blind.py
+++ blind.py
@@ -10,3 +10,18 @@ def find_highest_bidder(bidding_record):
             winner = bidder
     print(f"The winner is {winner} with a bid of ${highest_bid}")
 
+
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
+
